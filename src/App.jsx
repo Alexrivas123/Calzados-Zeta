789a18ed5696c94afed0bcb6a54b9c72437d8102
edward <zambrano.edward123@gmail.com>
2024-07-03 08:26:11 -0500

    inicio de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Usuario/>
+    </>
+  )
+}
+
+      const Usuario = () => {
+        return (
+
+              <div>
+            <h2>Edward Zambrano </h2>
+            <p> edad 26 </p>
+            <p> Nacionalidad: Venezolano</p>
+              </div>
+
+        )
+      }
+
+  
+
+
+export default App
